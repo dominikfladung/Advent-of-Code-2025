d7f1acb57ce6cd6109287f4a46a6433132281916
Dominik Fladung <dominik.fladung@codecentric.de>
2025-12-01 17:21:27 +0100

    feat: add 1. Advent of Code 2025 puzzle notebook

--- src/adventOfCode/helpers.py
+++ src/adventOfCode/helpers.py
@@ -1,10 +1,12 @@
 """Utility functions for the project."""
+from adventOfCode.paths import INPUTS_DIR
+
 
 def binary_to_decimal(binary: str) -> int:
     """Converts a binary number to a decimal number."""
     return int(binary, 2)
 
-def import_input(file_name: str) -> str:
+def import_input(file_name: int | str) -> list[str]:
     """Reads a file and returns its content."""
-    with open(f"./inputs/{str(file_name)}.txt", encoding="utf-8") as f:
-        return f.read().splitlines();
+    with open(INPUTS_DIR / f"{str(file_name)}.txt", encoding="utf-8") as f:
+        return f.read().splitlines()
